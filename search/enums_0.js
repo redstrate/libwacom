9d8af1b062cc1168c5b5e4122515a0f1af76d3bd
redstrate <redstrate@users.noreply.github.com>
2025-08-02 11:46:52 +0000

    deploy: 33e72d5d5011f97d00f55e68744d05c9ab67a23c

---
+++ search/enums_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['aliasstatus_0',['AliasStatus',['../libwacom_8h.html#ab1378b52d354d3deee585f4eccd24b6b',1,'libwacom.h']]]
+];
---
+++ search/enumvalues_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['ignore_5faliases_0',['IGNORE_ALIASES',['../libwacom_8h.html#ab1378b52d354d3deee585f4eccd24b6bab6e40c96bc0e5ae22b49d79fdf0ad4c7',1,'libwacom.h']]]
+];
